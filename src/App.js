58f10b73c1f01d8fee5acf233f7b9a9ae0274235
salaudeen olanrewaju <olanrewajulanzy@gmail.com>
2024-02-28 21:34:33 +0100

    commit message

--- src/App.js
+++ src/App.js
@@ -10,10 +10,14 @@ import pinterest from "./Images/Pinterest.png";
 import twitch from "./Images/Twitch.png";
 import webflow from "./Images/Webflow - Copy.png";
 import usersimage from "./Images/users-guide-image.png";
+import signup from "./Images/sign-up.png";
+import account from "./Images/account.png";
+import hospital from "./Images/hospital.png";
 import "./NavBar.css";
 import "./MainPage.css";
 import "./SocialIcons.css";
 import "./UsersGuide.css";
+import "./HospitalList.css";
 
 export default function App() {
   return (
@@ -22,6 +26,7 @@ export default function App() {
       <MainPage />
       <SocialIcons />
       <UsersGuide />
+      <HospitalLists />
     </div>
   );
 }
@@ -197,5 +202,63 @@ function UsersGuide() {
         </div>
       </div>
     </section>
+  );
+}
+
+function HospitalLists() {
+  return (
+    <article className="hospital-wrapper">
+      <div className="hospital-container">
+        <div className="hospital-about">
+          <h1>Got a Hospital you want to list?</h1>
+          <p>
+            We could assist you in placing your hospital to get shown when
+            searched for in 3 simple steps
+          </p>
+        </div>
+        <div className="hospital-features">
+          <div className="feature">
+            <img src={signup} alt="stethoscope" className="feature-image" />
+            <h3>Sign-Up with us</h3>
+            <p>
+              We could assist you in placing your hospital to get shown when
+              searched for in 3 simple steps
+            </p>
+          </div>
+          <div className="feature">
+            <img
+              src={account}
+              alt="doctor-patient interaction"
+              className="feature-image"
+            />
+            <h3>Create an account</h3>
+            <p>
+              Find all relevant sections, to populate the fields and get your
+              hospital listed on our platform
+            </p>
+          </div>
+          <div className="feature">
+            <img
+              src={hospital}
+              alt="hospital hallway"
+              className="feature-image"
+            />
+            <h3>List your hospital</h3>
+            <p>
+              Follow the prompts and fill all relevant sections, and get your
+              hospital listed as soon as we approve
+            </p>
+          </div>
+        </div>
+        <div className="feature-button">
+          <button className="btn form-btn">Get started now &rarr;</button>
+          <p>
+            <span>&#128337;</span>
+            10 minutes to complete
+          </p>
+        </div>
+        {/* <span className="counter"></span> */}
+      </div>
+    </article>
   );
 }
