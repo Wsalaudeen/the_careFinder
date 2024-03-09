14be2733ad5b8d8b588e204f22e87f13c057f449
salaudeen olanrewaju <olanrewajulanzy@gmail.com>
2024-03-09 09:43:27 +0100

    routing : sign-up-page

--- src/Pages/SignUpForm/SignUpForm.js
+++ src/Pages/SignUpForm/SignUpForm.js
@@ -1,5 +1,19 @@
 import "./SignUpForm.css";
+import logo from "../../Images/navbar_bar.png";
+import { FaFacebook, FaGoogle, FaApple } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
 
 export default function SignUpForm() {
-  return <h1> Ridwan is Agba coder</h1>;
+  return (
+    <section>
+      <div className="sign-up-logo">
+        <img src={logo} alt="careFinder" className="sign-up-image" />
+      </div>
+      <div className="sign-up-form">
+        <h1> Create Account</h1>
+        <p>Sign up to get search for hospitals near you super fast!</p>
+      </div>
+      <div className="sign-up-input"></div>
+    </section>
+  );
 }
