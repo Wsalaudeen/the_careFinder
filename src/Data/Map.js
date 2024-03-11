2e928cbc732a1fdb54ddf9132929e3de91546918
salaudeen olanrewaju <olanrewajulanzy@gmail.com>
2024-03-11 11:02:19 +0100

    vercel working

--- src/Data/Map.js
+++ src/Data/Map.js
@@ -9,7 +9,7 @@ export default function Map() {
           className="map-iframe"
           id="googgle-map"
           width="520"
-          // height="480"
+          height="480"
           src="https://maps.google.com/maps?width=650&amp;height=500&amp;hl=en&amp;q=Lagos,%20Nigeria+(CareFinder)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
         >
           {/* <a href="https://www.gps.ie/">hospital locator</a> */}
