57e38efab80914ee6fa102f8eb302203e87661b2
salaudeen olanrewaju <olanrewajulanzy@gmail.com>
2024-03-19 10:36:12 +0100

    complete the oauth functionality

--- src/components/Oauth/Oauth.js
+++ src/components/Oauth/Oauth.js
@@ -1,9 +1,36 @@
 import { FcGoogle } from "react-icons/fc";
 import "./Oauth.css";
+import { toast } from "react-toastify";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { doc, serverTimestamp, getDoc, setDoc } from "firebase/firestore";
+import { db } from "../../firebase";
+import { useNavigate } from "react-router-dom";
 
 export default function Oauth() {
+  async function handleGoogleClick() {
+    const navigate = useNavigate();
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      //  check for the user
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+      navigate("/");
+    } catch (error) {
+      toast.error("could not authorize with Google");
+    }
+  }
   return (
-    <button className="oauth">
+    <button onClick={handleGoogleClick} className="oauth" type="button">
       <FcGoogle className="google" /> Continue With Google
     </button>
   );
